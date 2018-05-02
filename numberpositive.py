6ea379500da835e589160c4d544038c63e2c2f00
Stephen Gushue <sgushue@gmail.com>
2018-05-01 20:00:33 -0700

    Assignments 1 and 2 from the homework

---
+++ numberpositive.py
@@ -0,0 +1,8 @@
+my_num = int(input('Please give me a number '))
+
+if my_num < 0:
+    print('Such a negative nancy!')
+elif my_num == 0:
+    print('Life is not a zero sum game!')
+else:
+    print ("Look who's a positive patty!")
---
+++ numbersize.py
@@ -0,0 +1,7 @@
+first_input = int(input('Give me a number '))
+second_input = int(input('Okay, now give me another number '))
+
+if first_input > second_input:
+    print ('Your first number is larger')
+else:
+    print('Your second number is larger')
